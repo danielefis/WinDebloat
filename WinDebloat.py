8439b46ee094b38801e2b918955bd5d1215469d9
Daniel <94140639+danielefis@users.noreply.github.com>
2022-07-01 16:12:22 -0300

    Update WinDebloat.py
    Novas opções adicionadas.

--- WinDebloat.py
+++ WinDebloat.py
@@ -5,7 +5,6 @@ import subprocess
 Script para remover manualmente aplicativos padrão Windows 10/11
 '''
 
-
 print("Você removerá manualmente aplicações e pacotes com [1- para SIM] e [2- para NÃO].")
 manual = int(input("Continuar? [1-SIM] [2-NÃO]: "))
 if manual == 1:
@@ -22,6 +21,14 @@ if manual == 1:
 manual = int(input("Remover Mapas? [1-SIM] [2-NÃO]: "))
 if manual == 1:
     subprocess.Popen(["powershell","Get-AppxPackage *WindowsMaps* | Remove-AppxPackage"])
+
+manual = int(input("Remover Vincular ao Celular? [1-SIM] [2-NÃO]: "))
+if manual == 1:
+    subprocess.Popen(["powershell","Get-AppxPackage *YourPhone* | Remove-AppxPackage"])
+
+manual = int(input("Remover Xbox e aplicativos relacionados? [1-SIM] [2-NÃO]: "))
+if manual == 1:
+    subprocess.Popen(["powershell","Get-AppxPackage *Xbox* | Remove-AppxPackage"])
 
 manual = int(input("Remover Microsoft To Do? [1-SIM] [2-NÃO]: "))
 if manual == 1:
@@ -51,6 +58,10 @@ manual = int(input("Remover Bloco de Notas? [1-SIM] [2-NÃO]: "))
 if manual == 1:
     subprocess.Popen(["powershell","Get-AppxPackage *WindowsNotepad* | Remove-AppxPackage"])
 
+manual = int(input("Remover Clima? [1-SIM] [2-NÃO]: "))
+if manual == 1:
+    subprocess.Popen(["powershell","Get-AppxPackage *BingWeather* | Remove-AppxPackage"])
+
 manual = int(input("Remover OneDrive? [1-SIM] [2-NÃO]: "))
 if manual == 1:
     subprocess.Popen(["powershell","Get-AppxPackage *OneDriveSync* | Remove-AppxPackage"])
@@ -61,4 +72,8 @@ if manual == 1:
 
 manual = int(input("Remover Teams/Chat? [1-SIM] [2-NÃO]: "))
 if manual == 1:
-    subprocess.Popen(["powershell","	Get-AppxPackage *Teams* | Remove-AppxPackage"])
+    subprocess.Popen(["powershell","Get-AppxPackage *Teams* | Remove-AppxPackage"])
+
+manual = int(input("Remover Notas Autoadesivas? [1-SIM] [2-NÃO]: "))
+if manual == 1:
+    subprocess.Popen(["powershell","Get-AppxPackage *MicrosoftStickyNotes* | Remove-AppxPackage"])
